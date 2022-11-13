948a3b0a2fe506ea90317fbf21ead493d94b3353
eve-69 <118100646+eve-69@users.noreply.github.com>
2022-11-13 14:38:33 +0530

    Update Huffman.py

--- Huffman.py
+++ Huffman.py
@@ -1,33 +1,60 @@
+# Huffman Coding in python
+
 string = 'BCAADDDCCACACAC'
 
+
+# Creating tree nodes
 class NodeTree(object):
 
     def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
-        
 
-class Item:
-	def __init__(self, value, weight):
-		self.value = value
-		self.weight = weight
+    def children(self):
+        return (self.left, self.right)
 
-def fractionalKnapsack(W, arr):
+    def nodes(self):
+        return (self.left, self.right)
 
-	arr.sort(key=lambda x: (x.value/x.weight), reverse=True)
-
-	finalvalue = 0.0
-
-	for item in arr:
-
-		if item.weight <= W:
-			W -= item.weight
-			finalvalue += item.value
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
 
 
-		else:
-			finalvalue += item.value * W / item.weight
-			break
-	
+# Main function implementing huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
 
-	return finalvalue
+
+# Calculating frequency
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+print(' Char | Huffman code ')
+print('----------------------')
+for (char, frequency) in freq:
+    print(' %-4r |%12s' % (char, huffmanCode[char]))
