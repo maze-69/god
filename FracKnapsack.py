67afa40d5af05285102e08f0ad5432d71346566b
eve-69 <118100646+eve-69@users.noreply.github.com>
2022-11-14 16:11:51 +0530

    Update FracKnapsack.py

--- FracKnapsack.py
+++ FracKnapsack.py
@@ -1,44 +1,35 @@
-# Structure for an item which stores weight and
-# corresponding value of Item
-class Item:
-	def __init__(self, value, weight):
-		self.value = value
-		self.weight = weight
+def fractional_knapsack(value, weight, capacity):
 
-# Main greedy function to solve problem
-def fractionalKnapsack(W, arr):
+    index = list(range(len(value)))
 
-	# Sorting Item on basis of ratio
-	arr.sort(key=lambda x: (x.value/x.weight), reverse=True)
+    ratio = [v/w for v, w in zip(value, weight)]
 
-	# Result(value in Knapsack)
-	finalvalue = 0.0
+    index.sort(key=lambda i: ratio[i], reverse=True)
+ 
+    max_value = 0
+    fractions = [0]*len(value)
+    for i in index:
+        if weight[i] <= capacity:
+            fractions[i] = 1
+            max_value += value[i]
+            capacity -= weight[i]
+        else:
+            fractions[i] = capacity/weight[i]
+            max_value += value[i]*capacity/weight[i]
+            break
+ 
+    return max_value, fractions
+ 
+ 
+n = int(input('Enter number of items: '))
+value = input('Enter the values of the {} item(s) in order: '
+              .format(n)).split()
+value = [int(v) for v in value]
+weight = input('Enter the positive weights of the {} item(s) in order: '
+               .format(n)).split()
+weight = [int(w) for w in weight]
+capacity = int(input('Enter maximum weight: '))
 
-	# Looping through all Items
-	for item in arr:
-
-		# If adding Item won't overflow,
-		# add it completely
-		if item.weight <= W:
-			W -= item.weight
-			finalvalue += item.value
-
-		# If we can't add current Item,
-		# add fractional part of it
-		else:
-			finalvalue += item.value * W / item.weight
-			break
-	
-	# Returning final value
-	return finalvalue
-
-
-# Driver Code
-if __name__ == "__main__":
-
-	W = 50
-	arr = [Item(60, 10), Item(100, 20), Item(120, 30)]
-
-	# Function call
-	max_val = fractionalKnapsack(W, arr)
-	print(max_val)
+max_value, fractions = fractional_knapsack(value, weight, capacity)
+print('The maximum value of items that can be carried:', max_value)
+print('The fractions in which the items should be taken:', fractions)
