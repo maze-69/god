acb80f98e084b9aec6c33075f1bcaf41e64d4c01
maze-69 <118100646+maze-69@users.noreply.github.com>
2022-11-15 18:41:53 +0530

    Update FracKnapsack.py

--- FracKnapsack.py
+++ FracKnapsack.py
@@ -1,35 +1,40 @@
-def fractional_knapsack(value, weight, capacity):
+class Item:
+	def __init__(self, value, weight):
+		self.value = value
+		self.weight = weight
 
-    index = list(range(len(value)))
 
-    ratio = [v/w for v, w in zip(value, weight)]
+def fractionalKnapsack(W, arr):
 
-    index.sort(key=lambda i: ratio[i], reverse=True)
- 
-    max_value = 0
-    fractions = [0]*len(value)
-    for i in index:
-        if weight[i] <= capacity:
-            fractions[i] = 1
-            max_value += value[i]
-            capacity -= weight[i]
-        else:
-            fractions[i] = capacity/weight[i]
-            max_value += value[i]*capacity/weight[i]
-            break
- 
-    return max_value, fractions
- 
- 
-n = int(input('Enter number of items: '))
-value = input('Enter the values of the {} item(s) in order: '
-              .format(n)).split()
-value = [int(v) for v in value]
-weight = input('Enter the positive weights of the {} item(s) in order: '
-               .format(n)).split()
-weight = [int(w) for w in weight]
-capacity = int(input('Enter maximum weight: '))
+	
+	arr.sort(key=lambda x: (x.value/x.weight), reverse=True)
 
-max_value, fractions = fractional_knapsack(value, weight, capacity)
-print('The maximum value of items that can be carried:', max_value)
-print('The fractions in which the items should be taken:', fractions)
+	
+	finalvalue = 0.0
+
+	
+	for item in arr:
+
+		
+		if item.weight <= W:
+			W -= item.weight
+			finalvalue += item.value
+
+		
+		
+		else:
+			finalvalue += item.value * W / item.weight
+			break
+	
+	
+	return finalvalue
+
+
+if __name__ == "__main__":
+
+	W = 50
+	arr = [Item(60, 10), Item(100, 20), Item(120, 30)]
+
+	# Function call
+	max_val = fractionalKnapsack(W, arr)
+	print(max_val)
